eeb139cc4ad63edca27d7029bac6b7f93c21654d
Selma Brattli <selma@brattli.nu>
2024-04-11 12:55:12 +0200

    header basic done

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import styles from '../stylesheets/App.module.css'
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <nav className={styles.navWrapper}>
+      <div>
+        <img className={styles.image} src="/logo-512vh.png" alt="the T-shirt store Logo" />
+      </div>
+      <ul className={styles.ulWrapper}>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/'>Products</NavLink></li>
+        <li><NavLink to='/'>About</NavLink></li>
+        <li><NavLink to='/'>Contact</NavLink></li>
+      </ul>
+    </nav>
   )
 }
 
